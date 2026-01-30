ac517fb9dfae7533d447b526a140084c5494206b
Chome <chomeallan@gmail.com>
2026-01-30 20:08:00 +0300

    navbar created

---
+++ src/Components/Tickets.jsx
@@ -0,0 +1,14 @@
+export function Tickets() {
+  return (
+    <div className="tickets-section">
+      <h2>My Tickets</h2>
+     
+        <div className="ticket-detail">
+          <p><strong>Movie:</strong></p>
+          <p><strong>Price:</strong></p>
+          <p>Your ticket has been booked! Check your email for confirmation.</p>
+        </div>
+    
+    </div>
+  )
+}
